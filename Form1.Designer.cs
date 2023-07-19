110d38258db3943bd6f9b30c1b59fe2bff3e0047
edu1107zera <128429561+edu1107zera@users.noreply.github.com>
2023-07-19 13:36:09 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace Aula07fase2Funcoes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 55);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mostra mensagem";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(33, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "mostra outra coisa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 202);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(196, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(442, 135);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(30, 13);
+            this.lblValor.TabIndex = 3;
+            this.lblValor.Text = "R$ 0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(445, 170);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(115, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(445, 213);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(94, 38);
+            this.btnSalvar.TabIndex = 5;
+            this.btnSalvar.Text = "sALVAR";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSalvar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula07fase2Funcoes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      
+        //Minhas funçoes
+        void mostrarMensagem()
+        {
+            MessageBox.Show("ola seja bem-vindo");
+            MessageBox.Show("lalala");
+          
+        }
+        void mostrarNome()
+        {
+            string nome = textBox1.Text;
+            MessageBox.Show("ola " +nome);
+      
+        }
+
+        Double converteDinheiro(Label dindin)
+        {
+            double dinheiro = double.Parse(dindin.Text.Replace("R$",""));//transforma label1 label2 label3...etc em dindin e acha na label a escrita itada (R$) e substitui ela por nada(VAZIO)
+            return dinheiro;
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mostrarMensagem();
+            mostrarNome();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mostrarMensagem();
+           
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            double valor = double.Parse(textBox2.Text);
+           double dinheiro=converteDinheiro(lblValor);
+            double soma = valor + dinheiro;
+            lblValor.Text = "R$"+soma;
+
+        }
+    }
+}
